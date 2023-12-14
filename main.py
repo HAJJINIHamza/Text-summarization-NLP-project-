2778e252a0c7e7c6b1ea7b5a983d8737a9354884
HAJJINIHamza <hajjinihamza2@gmail.com>
2023-12-14 15:45:55 +0100

    Add some utils functions

---
+++ main.py
@@ -0,0 +1,9 @@
+from textSummarizer.logging import logger
+from textSummarizer.utils.common import get_size
+
+path = 'src/textSummarizer/utils/common.py'
+print ('size of ', path, 'file is :', get_size (path))
+
+logger.info ( f"Getting size of {path}" )
+
+
